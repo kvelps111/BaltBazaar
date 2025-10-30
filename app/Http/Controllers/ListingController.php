90dad62b3c918fee1422bfaab908fdb607aaec82
Kristers <kvelps@gmail.com>
2025-10-30 15:25:30 +0200

    Index view, method, route

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,5 +11,23 @@ use App\Http\Requests\StoreListingRequest;
 
 class ListingController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        $filters = [
+            'region' => request('region'),
+            'school' => request('school'),
+            'category' => request('category')
+        ];
+
+        return view('listings.index', [
+            'listings' => Listing::with('school', 'photos')
+                ->latest()
+                ->filter($filters)
+                ->paginate(10),
+            'regions' => School::distinct()->pluck('region'),
+            'schools' => School::all(),
+            'categories' => Category::all()
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Listing routes
+    Route::get('/listings', [ListingController::class, 'index'])->name('listings.index');
+
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
