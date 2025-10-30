8f1b43e08b5b715449af5843a9ef7d240464e33c
Kristers <kvelps@gmail.com>
2025-10-30 14:27:14 +0200

    Listing model, migration, controller

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Listing;
+use App\Models\School;
+use App\Models\Category;
+use App\Http\Requests\StoreListingRequest;
+
+
+class ListingController extends Controller
+{
+    //
+}
